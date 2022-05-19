bdb976eb6a79316c3f0df41b86d3274c2728989c
Johannes Jungkunst <kaffeemann@outlook.com>
2022-05-19 13:17:29 +0200

    fixed impl

--- api/server/dnsrecord.go
+++ api/server/dnsrecord.go
@@ -13,7 +13,7 @@ type DNSRecord struct {
 	Uuid string `json:"uuid"`
 }
 
-func (s *DNSRecordService) GetDNSRecord(w http.ResponseWriter, r *http.Request) {
+func (s *DNSRecordService) GetItem(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	uuid := vars["uuid"]
 	if uuid == "" {
@@ -23,13 +23,21 @@ func (s *DNSRecordService) GetDNSRecord(w http.ResponseWriter, r *http.Request) 
 
 	s.RLock()
 	defer s.RUnlock()
+	if !s.itemExists(uuid) {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
 
-	dnsRecord := &DNSRecord{"test2", "5678"}
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(dnsRecord)
-	//err := json.NewEncoder(w).Encode(s.dnsRecords[uuid])
+	err := json.NewEncoder(w).Encode(s.dnsRecords[uuid])
 	if err != nil {
 		log.Println(err)
 		return
 	}
+}
+
+func (s *DNSRecordService) itemExists(uuid string) bool {
+	if _, ok := s.dnsRecords[uuid]; ok {
+		return true
+	}
+	return false
 }
